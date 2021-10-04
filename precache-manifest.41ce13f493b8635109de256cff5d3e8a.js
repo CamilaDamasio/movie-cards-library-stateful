5ff1dd37e42ffd8a2dd2a1b9f752e60b00793f53
CamilaDamasio <camiladamasioef@hotmail.com>
2021-10-04 18:44:43 -0300

    Updates

---
+++ precache-manifest.41ce13f493b8635109de256cff5d3e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe413682bda4bb2bc492d46a7cb4d26",
+    "url": "/mcl-stateful/index.html"
+  },
+  {
+    "revision": "14e0eb5b9f815ea45bc1",
+    "url": "/mcl-stateful/static/css/main.473a7ebb.chunk.css"
+  },
+  {
+    "revision": "6a4179d162e6ff4faded",
+    "url": "/mcl-stateful/static/js/2.711ad2a9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mcl-stateful/static/js/2.711ad2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e0eb5b9f815ea45bc1",
+    "url": "/mcl-stateful/static/js/main.46ed81fd.chunk.js"
+  },
+  {
+    "revision": "1b85bb1a38d521f7dcfe",
+    "url": "/mcl-stateful/static/js/runtime-main.0e0d1191.js"
+  }
+]);
